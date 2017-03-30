145961ab0214219204c70127235e48682d0cdbbe
Monica Howard <jay.m.howard@gmail.com>
2017-03-30 00:11:09 -0500

    added instruct. and other small changes

--- day-12-change-text-on-click/script.js
+++ day-12-change-text-on-click/script.js
@@ -2,5 +2,5 @@ var snakeQuote = document.querySelector('#snake-Quote');
 
 snakeQuote.addEventListener('click', function() {
   snakeQuote.textContent = '"I have had it with these precious little snakes on this precious little plane"';
-  snakeQuote.classList.toggle('on');
+
 });
