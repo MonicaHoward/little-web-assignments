327bfe2dbcb336300f67bf2eea1d0d4375a9fbed
Monica Howard <jay.m.howard@gmail.com>
2017-04-03 20:52:37 -0500

    setup before js toggle added

--- day-16-multi-toggle-on-click/script.js
+++ day-16-multi-toggle-on-click/script.js
@@ -1,6 +1,24 @@
-var toggleThisButton = document.querySelector('#bubble');
 
-  toggleThisButton.addEventListener('click', function(){
+
+
+
+  //toggleThisButton.addEventListener('click', function(){
+
+    //var eachButton = document.getElementsByClassName('.bubble');
+
+var toggleThisButton = document.querySelectorAll('.bubble');
+
+
+    for (var i = 0; i < bubble.length; i++); {
+
+
+
+    toggleThisButton[i].addEventListener('click', function() {
+
+
+
+}
+
     toggleThisButton.classList.toggle('on');
 
   });
