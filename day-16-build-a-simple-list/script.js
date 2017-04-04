d546b21c60e39d056403baae771ab0e87039256c
Monica Howard <jay.m.howard@gmail.com>
2017-04-04 08:45:13 -0500

    minor changes to two toggle projects

---
+++ day-16-build-a-simple-list/script.js
@@ -0,0 +1,24 @@
+var theList = document.querySelector('#the-list');
+
+var groceryList = [
+  'sausage',
+  'shrimp',
+  'Texjoy',
+  'Dr Pepper',
+  'steak',
+  'Tostitos',
+  'cheese',
+  'gum'
+];
+
+for (var = i; i < groceryList.length; i++) {
+
+  var li = document.createElement('li');
+
+   groceryList.appendChild(li);
+
+
+
+
+
+}
