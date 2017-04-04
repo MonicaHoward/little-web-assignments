aaade02ed3d3c2b6bc52caade93ff960de0df86e
Monica Howard <jay.m.howard@gmail.com>
2017-04-04 10:04:23 -0500

    minor changes

--- day-16-multi-toggle-on-click/script.js
+++ day-16-multi-toggle-on-click/script.js
@@ -6,26 +6,36 @@
 
     //var eachButton = document.getElementsByClassName('.bubble');
 
-var toggleThisButton = document.querySelectorAll('.bubble');
+///var toggleThisButton = document.querySelectorAll('.bubble');
 
 
-    for (var i = 0; i < toggleThisButton[i].length; i++); {
+//  for (var i = 0; i < toggleThisButton.length; i++); {
 
 
-    toggleThisButton[i].addEventListener('click', function(){
+//    toggleThisButton[i].addEventListener('click', function(){
 
-      toggleThisButton[i].classList.toggle('on');
-    });
+  //  toggleThisButton[i].classList.toggle('on');
+    //});
 
-    };
-
-
-
-
-
+//  };
 
 
 
   //  toggleThisButton[i].addEventListener('click', function() {
 
 //  toggleThisButton.classList.toggle('on');
+
+
+var theButton = document.querySelector('#add-thing');
+var ol =
+
+theButton.addEventListener('click' function () {
+
+  var li = document.createElement('li');
+
+  li.textContent = 'new';
+
+  var ol = document.querySelector('ol');
+
+  ol.appendChild(li);
+})
