2812e1238412f14daf98e8fc9e9276a9f972ea45
Monica Howard <jay.m.howard@gmail.com>
2017-04-05 08:51:56 -0500

    finished toggle assignment

--- day-16-multi-toggle-on-click/script.js
+++ day-16-multi-toggle-on-click/script.js
@@ -1,50 +1,8 @@
+var bubbleHolder = document.querySelector('.div-container');
+bubbleHolder.addEventListener('click', function(evt) {
+    console.log('clicked', evt);
+  if (evt.target.tagName === 'LI') {
+    evt.target.classList.toggle('on');
 
-
-
-
-  //toggleThisButton.addEventListener('click', function(){
-
-    //var eachButton = document.getElementsByClassName('.bubble');
-
-///var toggleThisButton = document.querySelectorAll('.bubble');
-
-
-//  for (var i = 0; i < toggleThisButton.length; i++); {
-
-
-//    toggleThisButton[i].addEventListener('click', function(){
-
-  //  toggleThisButton[i].classList.toggle('on');
-    //});
-
-//  };
-
-
-
-  //  toggleThisButton[i].addEventListener('click', function() {
-
-//  toggleThisButton.classList.toggle('on');
-
-
-var theButton = document.querySelector('#add-thing');
-var ol = document.querySelector('ol');
-
-
-ol.addEventListener('click', function(evt) {
-  console.log(evt);
-  if(evt.target.tagname === 'LI'){
-    
   }
-  evt.target.classList.toggle('on');
 });
-
-theButton.addEventListener('click' function () {
-
-  var li = document.createElement('li');
-
-  li.textContent = 'new';
-
-  var ol = document.querySelector('ol');
-
-  ol.appendChild(li);
-})
