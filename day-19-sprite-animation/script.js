5f11c6687148ce08e70532c99e809a4fd064f8ea
Monica Howard <jay.m.howard@gmail.com>
2017-04-07 15:39:31 -0500

    small updates

---
+++ day-19-sprite-animation/script.js
@@ -0,0 +1,6 @@
+var spriteSheet = document.querySelector('#sprite-sheet');
+var fowardButton = document.querySelector('#foward-button');
+var stopButton = document.querySelector('#stop-button');
+
+var positionX =
+var positionY =
