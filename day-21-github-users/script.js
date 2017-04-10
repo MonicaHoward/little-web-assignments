b21acf2a830923cfca2a97fcf4ffb80a51a7f609
Monica Howard <jay.m.howard@gmail.com>
2017-04-10 11:56:55 -0500

    a few changes

---
+++ day-21-github-users/script.js
@@ -0,0 +1,9 @@
+$.ajax ({
+  url: 'https://api.github.com/search/users?q=supermariobro'
+});
+
+var githubUsersMario = document.querySelector('#github-users');
+
+for (var i=0; i<       0             ; i++) {
+
+}
