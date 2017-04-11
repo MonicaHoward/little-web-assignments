dd20efdcc1b2b5e10424eba82da5817f9d67f3dd
Monica Howard <jay.m.howard@gmail.com>
2017-04-11 16:59:54 -0500

    mostly working

---
+++ day-22-scorecard/script.js
@@ -0,0 +1,21 @@
+var fs = require('fs');
+
+var fileData = fs.readFileSync('scorecard.csv', 'utf-8');
+
+console.log('---------------------------------------');
+console.log('75th Percentile ACT ScoreCard by School');
+console.log('---------------------------------------');
+
+var lines = fileData.split('\r');
+
+for (var i = 1; i < lines.length - 1; i++) {
+  var line = lines[i];
+  var pieces = line.split(',');
+  if (pieces[3])
+  console.log('Institution Name - ', pieces[3]);
+
+  if (pieces[32] !== 'NULL') {
+
+  console.log('75% CUMULATIVE ACT SCORE - ', pieces[33]);
+}
+}
