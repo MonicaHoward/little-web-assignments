518ac07898741f0b84d9478899d3e21f22bbeb73
Monica Howard <jay.m.howard@gmail.com>
2017-04-11 10:31:42 -0500

    updates

--- day-22-input/script.js
+++ day-22-input/script.js
@@ -9,15 +9,37 @@ var foods = [
 
 var foodListOL = document.querySelector('#food-list');
 
+var input = document.querySelector('#query');
+
+createList(foods);
+
+input.addEventListener('keyup'. function() {
+
+  var newList = [];
+
+
 for (var i = 0; i < foods.length; i++) {
 
-  var li = document.createElement('li');
-
-  li.textContent = foods[i];
-
-  foodListOL.appendChild(li);
-
+  if (foods[i].indexOf(input.value) > -1) {
+    newList.push(foods[i]);
+  }
 }
+
+  foodListOL.innerHTML = '';
+
+  createList(newList);
+
+)};
+
+
+function CreateList(arr {
+
+  for (var i = 0; i < arr.length; i++) {
+    
+  }
+})
+
+
 
 input.addEventListener('keyup', function() {
   console.log('keyup' input.value);
