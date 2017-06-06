7ee4975fe4eb730f191c40e6e9ea5d91f498c7e7
Monica Howard <jay.m.howard@gmail.com>
2017-06-06 11:39:53 -0500

    changes to slideshow appearance

--- day-19-simple-slide-show/script.js
+++ day-19-simple-slide-show/script.js
@@ -1,48 +1,17 @@
 var backButton = document.querySelector('.back-button');
 var nextButton = document.querySelector('.next-button');
 var playButton = document.querySelector('.play-button');
-var  slideShow = document.querySelector('.slide-show');
-
+ var slideShow = document.querySelector('.slide-show');
 
 var currentImage = 0;
 
-
 var images = [
-
 'images/chuck-norris-death.jpg',
-
 'images/chuck-norris-guns.jpg',
-
 'image/chuck-norris-password.jpg'
-
 ];
-  document.getElementById("slide-show").src="images/";
-//slideShow.src = showImages[0];
 
-nextButton.addEventListener('click', function (){
-  
+backButton.addEventListener('click', function(evt));
 
-/*  for (var i = 0; i < showImages.length; i++) {
-
-  currentImage += 1;
-
-    if (currentImage === showImages.length) {
-
-      currentImage = 0;
-    }
-  }
-  slideShow.src = showImages[currentImage];
-});
-
-console.log(slideShow);
-
-//previousButton.addEventListener('click', function (){
-
-  //currentImage -= 1;
-
-//  if (currentImage === 0) {
-  //  currentImage === showImages.length;
-
-//});
-
-*/
+nextButton.addEventListener('click');
+playButton.addEventListener('click')
